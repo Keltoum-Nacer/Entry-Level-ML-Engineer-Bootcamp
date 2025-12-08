d2b638abd96b0d64c9b69a8501584117ca413112
Keltoum Nacer <knacer@c4r9p6.1337.ma>
2025-12-08 19:56:22 +0100

    working on ex4

---
+++ Week00/ex04/kata00.py
@@ -0,0 +1,5 @@
+kata = (19,42,21)
+n = len(kata)
+a, b, c = kata
+msg = "The {} numbers are: {}, {}, {}".format(n, a, b, c)
+print(msg)
---
+++ Week00/ex04/kata02.py
@@ -0,0 +1,4 @@
+kata = (2019, 9, 25, 3, 30)
+
+a, b, c, d, e = kata
+print("0{}/{}/{} 0{}:{}$".format(b, c, a, d, e))
